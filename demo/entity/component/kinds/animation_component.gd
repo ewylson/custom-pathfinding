34c19ec30a00c9184c6b05fdc526d1ff19a50984
Ewilson <52111043+Ev1ls0n@users.noreply.github.com>
2025-07-29 17:16:35 +0300

    [feat] add desired Y velocity for facing right in AnimationComponent

--- demo/entity/component/kinds/animation_component.gd
+++ demo/entity/component/kinds/animation_component.gd
@@ -3,10 +3,13 @@ class_name AnimationComponent
 extends EntityComponent
 
 
+const RIGHT_Y_VELOCITY : float = 10.0
+
+
 func _process(_delta: float) -> void:
 	var velocity : Vector2 = owner_entity.velocity
 	if velocity != Vector2.ZERO:
-		if velocity.x != 0:
+		if velocity.x != 0 and (velocity.y >= -RIGHT_Y_VELOCITY and velocity.y <= RIGHT_Y_VELOCITY):
 			owner_entity.animation_player.play(&"look_right")
 			owner_entity.sprite.flip_h = velocity.x < 0
 		elif velocity.y < 0:
