882de66133dd890b03833acfb913ba8501ef9efb
Ewilson <52111043+Ev1ls0n@users.noreply.github.com>
2025-08-07 18:25:56 +0300

    [feat] add flag to ignore solid cells in Pathfinder2D

--- pathfinding/pathfinder2d.gd
+++ pathfinding/pathfinder2d.gd
@@ -20,6 +20,7 @@ signal map_changed()
 @export var diagonal_mode : AStarGrid2D.DiagonalMode
 @export var simplify_path : bool = false
 @export var allow_partial_path : bool = true
+@export var allow_ignore_solid_cells : bool = false
 
 @export_group("Pathfinding Source")
 @export var source_pathfinding_mode : SourcePathfindingMode
@@ -151,7 +152,7 @@ func __find_solid_cells(layers: Array[TileMapLayer]) -> Array[Vector2i]:
 func __update_astar_grid() -> void:
 	__astar_grid.update()
 	for cell : Vector2i in pathfinding_map.solid_cells:
-		__astar_grid.set_point_solid(cell, true)
+		__astar_grid.set_point_solid(cell, not allow_ignore_solid_cells)
 	return
 
 
